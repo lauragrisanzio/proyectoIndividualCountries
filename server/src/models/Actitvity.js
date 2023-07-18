767fccfc9145b74477b0eaa564fe24a111ee5d8d
Laura Grisanzio <lauragrisanzio2@gmail.com>
2023-07-18 13:27:43 -0300

    models

---
+++ server/src/models/Actitvity.js
@@ -0,0 +1,50 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("Activity", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    difficulty: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: 1,
+        max: 5,
+        isEven(value) {
+          if (value < 1 || value > 5)
+            throw new Error("The difficulty must be between 1 and 5");
+        },
+      },
+      allowNull: false,
+    },
+    duration: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: 1,
+        max: 24,
+        isEven(value) {
+          if (value < 1 || value > 24)
+            throw new Error("The duration must be between 1 and 24");
+        },
+      },
+      allowNull: true,
+    },
+    season: {
+      type: DataTypes.ENUM("Summer", "Autumn", "Winter", "Spring"),
+      allowNull: false
+    }
+  }, {
+    timestamps: false,
+  });
+};
+
+
--- server/src/models/Country.js
+++ server/src/models/Country.js
@@ -3,10 +3,43 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Country', {
+  sequelize.define("Country", {
+    id: {
+      type: DataTypes.STRING(3),
+      allowNull: false,
+      unique: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subregion: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    area: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    population: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+    {
+    timestamps: false
+  }
+  );
 };
